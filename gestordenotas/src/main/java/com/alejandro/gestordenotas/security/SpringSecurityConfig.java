ce20f43f1a9943ac03994bbee4126b889f5921ed
aledelorbe <mgranadosb1800@alumno.ipn.mx>
2025-01-05 21:58:59 -0600

    implementation of login

--- gestordenotas/src/main/java/com/alejandro/gestordenotas/security/SpringSecurityConfig.java
+++ gestordenotas/src/main/java/com/alejandro/gestordenotas/security/SpringSecurityConfig.java
@@ -1,16 +1,24 @@
 package com.alejandro.gestordenotas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.alejandro.gestordenotas.security.filter.JwtAuthenticationFilter;
+
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     // To be able to encrypt passwords
     @Bean
@@ -18,16 +26,23 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // To use the login
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     // Method to config the security rules
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests( (authz) -> authz 
-            .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
-            .requestMatchers("/api/users").permitAll()
-            .anyRequest().authenticated())
-            .csrf(config -> config.disable())
-            .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .build();  
+        return http.authorizeHttpRequests((authz) -> authz
+                .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+                .requestMatchers("/api/users").permitAll()
+                .anyRequest().authenticated())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager())) // login
+                .csrf(config -> config.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
 }
